95a30e095e3013d0ba9466d0cfea3d24472738c0
Emi-ms <cabeemil86@gmail.com>
2024-04-27 14:32:32 +0200

    creando el proyecto

---
+++ src/main/java/org/iesbelen/saborenlared/SaborEnLaRedApplication.java
@@ -0,0 +1,13 @@
+package org.iesbelen.saborenlared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaborEnLaRedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaborEnLaRedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iesbelen/saborenlared/SaborEnLaRedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesbelen.saborenlared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaborEnLaRedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
