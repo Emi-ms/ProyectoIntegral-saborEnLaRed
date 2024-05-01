ba0caec9b90dc043ac0ec46e051b23e20a1d66ba
Emi-ms <cabeemil86@gmail.com>
2024-05-01 19:04:21 +0200

    FRONT: componente Home y Footer terminados

--- src/main/frontend/saborEnLaRed-app/src/app/app.component.ts
+++ src/main/frontend/saborEnLaRed-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TopBarComponent} from "./components/top-bar/top-bar.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopBarComponent],
+  imports: [RouterOutlet, TopBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
