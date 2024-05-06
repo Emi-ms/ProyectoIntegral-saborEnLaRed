9fe4a9781f743931923efc734bad16f75a0214ea
Emi-ms <cabeemil86@gmail.com>
2024-05-06 10:47:52 +0200

    Terminado módulo de registro de usuarios

--- src/main/frontend/saborEnLaRed-app/src/app/components/signup/signup.component.ts
+++ src/main/frontend/saborEnLaRed-app/src/app/components/signup/signup.component.ts
@@ -1,17 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {NgIf, NgOptimizedImage} from "@angular/common";
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInput} from "@angular/material/input";
-import {MatButton} from "@angular/material/button";
-import {RouterLink} from "@angular/router";
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
-import {UserService} from "../../services/user.service";
+import { Component, OnInit } from '@angular/core';
+import { NgIf, NgOptimizedImage } from "@angular/common";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput } from "@angular/material/input";
+import { MatButton } from "@angular/material/button";
+import { RouterLink } from "@angular/router";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { UserService } from "../../services/user.service";
 
-import { catchError } from 'rxjs/operators';
-import { Observable, throwError } from 'rxjs';
 import Swal from 'sweetalert2';
-import { nextTick } from 'process';
 
 @Component({
   selector: 'app-signup',
@@ -29,8 +26,6 @@ import { nextTick } from 'process';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent implements OnInit {
-
-
 
   constructor(
     public userService: UserService,
@@ -50,14 +45,14 @@ export class SignupComponent implements OnInit {
   });
 
 
- submit() {
+  submit() {
     console.log(this.form.value);
     this.userService.registerUser(this.form.value).subscribe({
       next: (res: any) => {
         console.log("en el next")
-          console.log('Usuario creado correctamente!');
-          Swal.fire("Enhorabuena!!","Usuario registrado con éxito!!", "success")
-          this.router.navigateByUrl('/')
+        console.log('Usuario creado correctamente!');
+        Swal.fire("Enhorabuena!!", "Usuario registrado con éxito!!", "success")
+        this.router.navigateByUrl('/')
           .then();
       },
       error: (error: any) => {
