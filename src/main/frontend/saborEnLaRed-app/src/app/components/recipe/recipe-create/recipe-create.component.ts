9dbe63ae5847507f0d4f2d2977f596f578cc68fc
Emi-ms <cabeemil86@gmail.com>
2024-05-23 19:30:55 +0200

    FRONT: Mejorando componente de crear Receta

--- src/main/frontend/saborEnLaRed-app/src/app/components/recipe/recipe-create/recipe-create.component.ts
+++ src/main/frontend/saborEnLaRed-app/src/app/components/recipe/recipe-create/recipe-create.component.ts
@@ -1,4 +1,4 @@
-import { NgForOf, NgIf, NgOptimizedImage } from '@angular/common';
+import { NgForOf, NgIf, NgOptimizedImage, AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
@@ -11,6 +11,13 @@ import { UserService } from '../../../services/user.service';
 import { IngredientService } from '../../../services/ingredient.service';
 import { CategoryService } from '../../../services/category.service';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { Observable, map, startWith } from 'rxjs';
+import {MatSelectModule} from '@angular/material/select';
+
 @Component({
   selector: 'app-recipe-create',
   standalone: true,
@@ -20,7 +27,13 @@ import { CategoryService } from '../../../services/category.service';
     NgOptimizedImage,
     RouterLink,
     NgForOf,
-    FormsModule
+    FormsModule,
+    MatButtonModule,
+    AsyncPipe,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   templateUrl: './recipe-create.component.html',
   styleUrls: ['./recipe-create.component.css']
@@ -29,9 +42,9 @@ export class RecipeCreateComponent implements OnInit {
   user: User | null | undefined;
   ingredientsFromAPI: Ingredient[] = [];
   categoriesFromAPI: Category[] = [];
-  selectedIngredientControl = new FormControl(null);
   selectedCategoryControl = new FormControl(null);
-
+  filteredIngredients: Observable<Ingredient[]> = new Observable<Ingredient[]>();
+  ingredientControl = new FormControl();
   recipeForm: FormGroup;
 
   constructor(
@@ -44,18 +57,24 @@ export class RecipeCreateComponent implements OnInit {
   ) {
     this.recipeForm = this.fb.group({
       recipeName: ['', [Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+'), Validators.maxLength(255)]],
-      description: ['',[Validators.required, Validators.maxLength(255)]],
+      description: ['', [Validators.required, Validators.maxLength(255)]],
       photo: ['', [Validators.required, Validators.maxLength(255)]],
       active: [true],
       recipeIngredients: this.fb.array([]),
       categories: this.fb.array([]),
       user: [this.userService.getUserFromSessionStorage()]
-       });
+    });
   }
 
   ngOnInit(): void {
     this.ingredientService.getAll().subscribe((data: Ingredient[]) => {
       this.ingredientsFromAPI = data;
+      this.filteredIngredients = this.ingredientControl.valueChanges.pipe(
+        startWith(''),
+        map(value => this._filter(value))
+
+
+      );
     });
 
     this.categoryService.getAll().subscribe((data: Category[]) => {
@@ -76,13 +95,12 @@ export class RecipeCreateComponent implements OnInit {
         ingredient: [ingredient, Validators.required],
         quantity: [quantityNumber, Validators.required],
         unitMeasure: [unitMeasure, Validators.required]
-
-      
       }));
 
       console.log(this.recipeIngredientsFromForm.value);
+      this.ingredientControl.reset();
     } else {
-      Swal.fire('Error', 'Please fill in all ingredient fields', 'error');
+      Swal.fire('Lo siento!!', 'Tienes que rellenar todos los campos del ingrediente', 'error');
     }
   }
 
@@ -96,10 +114,11 @@ export class RecipeCreateComponent implements OnInit {
         idCategory: [category.idCategory, Validators.required],
         categoryName: [category.categoryName, Validators.required],
         active: true
-      
-      
+
+
       }));
       console.log(this.categoriesFromForm.value);
+
     } else {
       Swal.fire('Error', 'Please select a category', 'error');
     }
@@ -109,14 +128,28 @@ export class RecipeCreateComponent implements OnInit {
     if (this.recipeForm.valid) {
       console.log(this.recipeForm.value);
       this.recipeService.save(this.recipeForm.value).subscribe(() => {
+        Swal.fire('Receta creada', 'Gracias por tu aportación cocinilla!!', 'success');
         this.router.navigate(['/recipes']);
       });
     } else {
-      Swal.fire('Error', 'Please fill in all required fields', 'error');
+      Swal.fire('Lo siento!!', 'Tienes que rellenar todos los campos', 'error');
     }
   }
 
   get f() {
     return this.recipeForm.controls;
+  }
+
+  displayFn(ingredient: Ingredient): string {
+    return ingredient && ingredient.ingredientName ? ingredient.ingredientName : '';
+  }
+
+  private _filter(value: string): Ingredient[] {
+    return this.ingredientsFromAPI.filter(ingredient => ingredient.ingredientName.toLowerCase().includes(value));
+  }
+
+  removeIngredient(index: number) {
+    this.recipeIngredientsFromForm.removeAt(index);
+    console.log(this.recipeIngredientsFromForm.value);
   }
 }
